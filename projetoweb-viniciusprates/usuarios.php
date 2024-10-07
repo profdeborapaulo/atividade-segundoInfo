6d8bde07dc38e44bb523e2e3f88bc5d9e1ed2524
V1niciusPr4tes <145776237+Vin1ciusPr4tes@users.noreply.github.com>
2024-10-07 10:45:33 -0300

    Add files via upload

--- projetoweb-viniciusprates/usuarios.php
+++ projetoweb-viniciusprates/usuarios.php
@@ -42,7 +42,6 @@
         }
     </style>
 </head>
-
 <body>
     <header>
         <h1>USUARIOS</h1>
@@ -55,16 +54,66 @@
         <form action="" id="form1">
            <label for="id">Pesquisa por ID:</label>
            <input type="text" name="id" id="id" required>
-           <button type="submit">Buscar</button>
+           <button type="submit" onclick="pesquisaid()">Buscar</button>
         </form>
         <form action="" id="form2">
             <label for="nome">Pesquisa por Nome</label>
             <input type="text" name="nome" id="nome" requird>
-            <button type="submit">Buscar</button>
+            <button type="submit" onclick="pesquisanome()">Buscar</button>
         </form><br>
     </div>
         <div id="botao"><button type="submit" id="att" onclick="location.reload()">Atualizar</button></div>
     
+<script>
+    function pesquisaid() {
+    // Impedir que o formulário recarregue a página
+    event.preventDefault();
+
+    // Obter o valor do ID pesquisado
+    var inputID = document.getElementById("id").value.toLowerCase();
+
+    // Obter todas as linhas da tabela
+    var rows = document.querySelectorAll("tbody tr");
+
+    // Percorrer todas as linhas da tabela
+    rows.forEach(function (row) {
+        // Obter a célula que contém o ID
+        var cellID = row.querySelector("td:first-child").textContent.toLowerCase();
+
+        // Verificar se o ID da célula corresponde ao valor pesquisado
+        if (cellID.includes(inputID)) {
+            row.style.display = ""; // Mostrar a linha
+        } else {
+            row.style.display = "none"; // Esconder a linha
+        }
+    });
+}
+
+function pesquisanome() {
+    // Impedir que o formulário recarregue a página
+    event.preventDefault();
+
+    // Obter o valor do nome pesquisado
+    var inputNome = document.getElementById("nome").value.toLowerCase();
+
+    // Obter todas as linhas da tabela
+    var rows = document.querySelectorAll("tbody tr");
+
+    // Percorrer todas as linhas da tabela
+    rows.forEach(function (row) {
+        // Obter a célula que contém o nome
+        var cellNome = row.querySelector("td:nth-child(2)").textContent.toLowerCase();
+
+        // Verificar se o nome da célula corresponde ao valor pesquisado
+        if (cellNome.includes(inputNome)) {
+            row.style.display = ""; // Mostrar a linha
+        } else {
+            row.style.display = "none"; // Esconder a linha
+        }
+    });
+}
+
+</script>
 
     <center>
         <table>
@@ -99,5 +148,4 @@
     </center>
    
 </body>
-
 </html>
