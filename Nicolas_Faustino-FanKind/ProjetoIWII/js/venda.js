c7e73b56d081e2daa10e3e077b171551092cfcb0
nicolas! <148554370+f4ustinozx@users.noreply.github.com>
2024-10-07 08:09:44 -0300

    Update venda.js

--- Nicolas_Faustino-FanKind/ProjetoIWII/js/venda.js
+++ Nicolas_Faustino-FanKind/ProjetoIWII/js/venda.js
@@ -1,14 +1,19 @@
-
 document.getElementById('anuncio-form').addEventListener('submit', function(event) {
     event.preventDefault(); 
 
-   
     const artista = document.getElementById('artista').value;
     const motivo = document.getElementById('motivo').value;
     const data = document.getElementById('data').value;
     const preco = document.getElementById('preco').value;
 
-    localStorage.setItem('anuncioData', JSON.stringify({ artista, motivo, data, preco }));
+    
+    let anuncios = JSON.parse(localStorage.getItem('anuncios')) || [];
+    
+    
+    anuncios.push({ artista, motivo, data, preco });
+
+    
+    localStorage.setItem('anuncios', JSON.stringify(anuncios));
 
     window.location.href = 'homevendedor.html';
 });
