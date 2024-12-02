907712648c8d1e12ff009796b37ec32531cfda11
SantosPereiraArthur <arthur.santos240@etec.sp.gov.br>
2024-12-02 07:48:17 -0300

    Meu trabalho sobre foguetes
    Comecei com dificuldade e sem gostar do tema, mas depois de pesquisar gostei

---
+++ Arthur_Pereira/registrar.js
@@ -0,0 +1,14 @@
+function enviar() {
+    window.location.href = "mercado.html";
+}
+function receber(){
+document.getElementById("formulario");
+let usuario = document.getElementById("nome").value;
+let email = document.getElementById("email").value;
+let senha = document.getElementById("senha").value;
+
+alert(usuario);
+alert(email);
+alert(senha);
+
+}
